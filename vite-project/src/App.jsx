e27155f68496e115ba3f16a2362721e0e5ecf3fe
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-06 21:24:10 +0330

    feat: home added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import Header from "./components/Header";
 import "./styles/main.css";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <div className="container">
         <Header />
+        <Home />
         <Footer />
       </div>
     </>
--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,5 +1,15 @@
+import "../styles/home.css";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+        <div className="home-container ">
+          <h2>Food Corner</h2>
+          <p>INDIAN FOOD AT A CLICK</p>
+          <button>ORDER NOW</button>
+        </div>
+      </div>
+  );
 };
 
 export default Home;
