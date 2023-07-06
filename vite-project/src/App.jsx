ab5ba5e14d31da62cea91fdd47bec1a69fec3c90
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-06 20:26:31 +0330

    feat: footer added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import "./styles/main.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <div className="container">
         <Header />
+        <Footer />
       </div>
     </>
   );
---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import "../styles/footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer__icons">
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-square-facebook"></i>
+        <i class="fa-brands fa-linkedin"></i>
+      </div>
+      <div className="footer__content">
+        <p>@2021 IndeanFooood.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
