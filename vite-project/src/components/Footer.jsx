fa3d2c09d10705a0372d0e0a8458b116a66deb51
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-07 17:54:10 +0330

    fix: icons class name fixed

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer__icons">
-        <i class="fa-brands fa-instagram"></i>
-        <i class="fa-brands fa-twitter"></i>
-        <i class="fa-brands fa-square-facebook"></i>
-        <i class="fa-brands fa-linkedin"></i>
+        <i className="fa-brands fa-instagram"></i>
+        <i className="fa-brands fa-twitter"></i>
+        <i className="fa-brands fa-square-facebook"></i>
+        <i className="fa-brands fa-linkedin"></i>
       </div>
       <div className="footer__content">
         <p>@2021 IndeanFooood.com</p>
