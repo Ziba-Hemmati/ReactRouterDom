acc0a2730957f838fe0ffd99f392edbe22685731
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-07 11:27:42 +0330

    feat: about page added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
-import Header from "./components/Header";
 import "./styles/main.css";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Menu from "./components/menu";
+import About from "./components/About";
+import Contact from "./components/contact";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className="container">
         <Header />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
         <Footer />
       </div>
     </>
--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -1,5 +1,29 @@
+import "../styles/about.css";
+
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="about-container">
+        <div className="about__image"></div>
+        <div className="about__content">
+          <h2>ABOUT US</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam aut
+            mollitia est exercitationem accusamus? Laborum doloremque soluta ut
+            illum nemo vitae omnis voluptatibus exercitationem ducimus
+            distinctio debitis, pariatur modi facere? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Quasi rerum a esse suscipit vitae
+            error fuga iusto, asperiores magnam doloremque iure debitis modi
+            excepturi tempore dicta. Officiis iure id vel? Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Consequuntur possimus
+            voluptatibus optio commodi laborum, nihil delectus. Beatae
+            exercitationem, illum nam, a fuga odio temporibus aut, amet facilis
+            ea sed accusamus.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
