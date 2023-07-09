4d99e14b5e1ce5cd85065b53fa8d96818bd89aca
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-09 19:31:41 +0330

    fix: icon's link fixed

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,21 +1,22 @@
+import { Link } from "react-router-dom";
 import "../styles/footer.css";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer__icons">
-        <a href="#">
+        <Link to="/">
           <i className="fa-brands fa-instagram"></i>
-        </a>
-        <a href="#">
+        </Link>
+        <Link to="/">
           <i className="fa-brands fa-twitter"></i>
-        </a>
-        <a href="#">
+        </Link>
+        <Link to="/">
           <i className="fa-brands fa-square-facebook"></i>
-        </a>
-        <a href="#">
+        </Link>
+        <Link to="/">
           <i className="fa-brands fa-linkedin"></i>
-        </a>
+        </Link>
       </div>
       <div className="footer__content">
         <p>@2021 IndeanFooood.com</p>
